3fa942d25d31f710b2661882fc8eb57a36ea372a
Roseoketch <oketchrose@gmail.com>
2018-05-20 20:55:48 +0300

    templates added

--- passenger/models.py
+++ passenger/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from django import forms
 
 # Create your models here.
 class Passenger(models.Model):
